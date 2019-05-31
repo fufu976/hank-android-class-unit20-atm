558b9a92eb7f1eb89d949c3cf733e988480f12b7
fufu976 <fufu976@gmail.com>
2019-05-31 11:56:25 +0800

    Unit 21 ATM Login with Firebase Realtime Database

--- app/src/main/java/com/kingde/atm/LoginActivity.java
+++ app/src/main/java/com/kingde/atm/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.kingde.atm;
 
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,11 +33,29 @@ public class LoginActivity extends AppCompatActivity {
 
     public void login(View view) {
         String username = edUsername.getText().toString();
-        String password = edPasswd.getText().toString();
-        if("admin".equals(username) && "admin".equals(password)) {
-            setResult(RESULT_OK);
-            finish();
-        }
+        final String password = edPasswd.getText().toString();
+        FirebaseDatabase.getInstance().getReference("users").child(username).child("password")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String pwd = (String) dataSnapshot.getValue();
+                        if(pwd.equals(password)) {
+                            setResult(RESULT_OK);
+                            finish();
+                        } else {
+                            new AlertDialog.Builder(LoginActivity.this)
+                                    .setTitle("Warning")
+                                    .setMessage("username or password incorrect")
+                                    .setPositiveButton("OK", null)
+                                    .show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 
     public void quit(View view) {
