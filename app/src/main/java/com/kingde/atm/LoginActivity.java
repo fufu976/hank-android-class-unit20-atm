d7616e41d290c8d41966703dce0a5c08d6e98980
fufu976 <fufu976@gmail.com>
2019-06-01 20:27:48 +0800

    Unit 23 Check Box Remember username or not

--- app/src/main/java/com/kingde/atm/LoginActivity.java
+++ app/src/main/java/com/kingde/atm/LoginActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = LoginActivity.class.getSimpleName();
     private EditText edUsername;
     private EditText edPasswd;
+    private CheckBox cbRemember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,19 @@ public class LoginActivity extends AppCompatActivity {
     private void findView() {
         edUsername = findViewById(R.id.username);
         edPasswd   = findViewById(R.id.passwd);
+        cbRemember = findViewById(R.id.checkBox);
+        cbRemember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getSharedPreferences("atm", MODE_PRIVATE)
+                        .edit()
+                        .putBoolean("REMEMBER_USERNAME", isChecked)
+                        .apply();
+            }
+        });
+        boolean bRemember = getSharedPreferences("atm", MODE_PRIVATE)
+                .getBoolean("REMEMBER_USERNAME", false);
+        cbRemember.setChecked(bRemember);
     }
 
     public void login(View view) {
@@ -47,13 +63,18 @@ public class LoginActivity extends AppCompatActivity {
                         String pwd = (String) dataSnapshot.getValue();
                         if(pwd.equals(password)) {
                             setResult(RESULT_OK);
+                            boolean bRemember = getSharedPreferences("atm", MODE_PRIVATE)
+                                    .getBoolean("REMEMBER_USERNAME", false);
+                            Log.d(TAG, "onDataChange: bRemember: " + bRemember);
+                            String username_t = "";
+                            if(bRemember) {
+                                username_t = username;
+                            }
+                            Log.d(TAG, "onDataChange: username_t: " + username_t);
                             getSharedPreferences("atm", MODE_PRIVATE)
                                     .edit()
-                                    .putString("USERNAME", username)
+                                    .putString("USERNAME", username_t)
                                     .commit();
-                            String username = getSharedPreferences("atm", MODE_PRIVATE)
-                                    .getString("USERNAME", "");
-                            Log.d(TAG, "login: username " + username);
                             finish();
                         } else {
                             new AlertDialog.Builder(LoginActivity.this)
